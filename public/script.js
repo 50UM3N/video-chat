4fdf2eb4f985cbaca23875de6512db88c294055b
Soumen Khara <soumen2015.s.k@gmail.com>
2021-01-08 12:00:34 +0530

    Initila commit and finish project

---
+++ public/script.js
@@ -0,0 +1,330 @@
+function detectMob() {
+  const toMatch = [
+    /Android/i,
+    /webOS/i,
+    /iPhone/i,
+    /iPad/i,
+    /iPod/i,
+    /BlackBerry/i,
+    /Windows Phone/i,
+  ];
+
+  return toMatch.some((toMatchItem) => {
+    return navigator.userAgent.match(toMatchItem);
+  });
+}
+if (detectMob()) {
+  const cont = document.querySelector("#share-screen");
+  cont.remove();
+}
+const socket = io("/");
+const videoGrid = document.getElementById("video-grid");
+// const name = prompt("Your name");
+const name = "Soumen Khara";
+const myPeer = new Peer(undefined, {
+  path: "/peerjs",
+  host: "/",
+  //  port: "3000",
+});
+var Peer_ID;
+const myVideo = document.createElement("video");
+console.log(myPeer);
+myVideo.muted = true;
+let myVideoStream;
+const peers = {};
+if (s) {
+  navigator.mediaDevices
+    .getDisplayMedia({
+      video: true,
+      audio: {
+        echoCancellation: true,
+        noiseSuppression: true,
+        sampleRate: 44100,
+      },
+    })
+    .then((stream) => {
+      myVideoStream = stream;
+      stream.getVideoTracks()[0].onended = () => {
+        location.replace(`/${ROOM_ID}`);
+      };
+      processStream(stream, 10000);
+    });
+} else {
+  navigator.mediaDevices
+    .getUserMedia({ audio: true })
+    .then((stream) => {
+      navigator.mediaDevices
+        .getUserMedia({
+          video: true,
+          audio: true,
+        })
+        .then((stream) => {
+          myVideoStream = stream;
+          processStream(stream, 0);
+        });
+    })
+    .catch((err) => {
+      console.log("Dont have microphone");
+      navigator.mediaDevices
+        .getUserMedia({
+          video: true,
+          audio: false,
+        })
+        .then((stream) => {
+          myVideoStream = stream;
+          processStream(stream, 0);
+        });
+    });
+}
+
+socket.on("user-disconnected", (userId) => {
+  if (peers[userId]) peers[userId].close();
+});
+
+function processStream(stream, timeOut) {
+  stream.name = "soumen";
+  addVideoStream(myVideo, stream, null, name);
+  // recieve the others stream
+  myPeer.on("call", (call) => {
+    peers[call.peer] = call;
+    call.answer(stream);
+    console.log(call);
+    const video = document.createElement("video");
+    call.on("stream", (userVideoStream) => {
+      fetch(`/user/${call.peer}`)
+        .then((res) => {
+          return res.json();
+        })
+        .then((data) => {
+          console.log(data);
+          addVideoStream(video, userVideoStream, call.peer, data.name);
+        });
+    });
+    call.on("close", () => {
+      video.parentElement.remove();
+    });
+  });
+
+  socket.on("user-connected", (userId) => {
+    console.log("user-connected");
+    connectToNewUser(userId, stream);
+  });
+}
+myPeer.on("open", (id) => {
+  Peer_ID = id;
+});
+function connectToNewUser(userId, stream) {
+  // set others peerid and send my stream
+  stream.name = "soumen";
+  const call = myPeer.call(userId, stream);
+  console.log(call);
+  const video = document.createElement("video");
+  call.on("stream", (userVideoStream) => {
+    fetch(`/user/${call.peer}`)
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data);
+        addVideoStream(video, userVideoStream, call.peer, data.name);
+      });
+  });
+  call.on("close", () => {
+    video.parentElement.remove();
+  });
+
+  peers[userId] = call;
+}
+
+function addVideoStream(video, stream, p, n) {
+  const videoWrapper = document.createElement("div");
+  videoWrapper.id = "video-wrapper";
+  videoWrapper.classList.add("video-wrapper");
+  const namePara = document.createElement("p");
+  namePara.innerHTML = n;
+  videoWrapper.appendChild(namePara);
+  video.srcObject = stream;
+  video.setAttribute("peer", p);
+  video.setAttribute("name", n);
+  video.addEventListener("loadedmetadata", () => {
+    video.play();
+  });
+  // pinch out button
+  // const pinchOutBtn = document.createElement("button");
+  // pinchOutBtn.classList.add("video-element-btn");
+  // pinchOutBtn.classList.add("video-pinch-out-btn");
+  // pinchOutBtn.innerHTML = `<i class="none-pointer fas fa-compress-arrows-alt fa-2x"></i>`;
+  // videoWrapper.appendChild(pinchOutBtn);
+  videoWrapper.appendChild(video);
+  console.log(videoWrapper.childNodes);
+  videoGrid.append(videoWrapper);
+  eventAdd(video);
+}
+
+function shareScreen() {
+  var Screen_Peer_ID;
+  const screenVideo = document.createElement("video");
+  screenVideo.muted = true;
+  location.replace(`/${ROOM_ID}?screen=true`);
+  // const screenPeer = new Peer(undefined, {
+  //   path: "/peerjs",
+  //   host: "/",
+  //   //  port: "3000",
+  // });
+  // navigator.mediaDevices
+  //   .getDisplayMedia({
+  //     video: true,
+  //     audio: {
+  //       echoCancellation: true,
+  //       noiseSuppression: true,
+  //       sampleRate: 44100,
+  //     },
+  //   })
+  //   .then((stream) => {
+  //     socket.emit("join-room-screen", ROOM_ID, Screen_Peer_ID, name);
+  //     stream.getVideoTracks()[0].onended = () => {
+  //       location.replace(`/${ROOM_ID}`);
+  //     };
+  //     addVideoStream(screenVideo, stream, null, name);
+  //     // recieve the others stream
+  //     screenPeer.on("call", (call) => {
+  //       peers[call.peer] = call;
+  //       call.answer(stream);
+  //       console.log(call);
+  //       const video = document.createElement("video");
+  //       call.on("stream", (userVideoStream) => {
+  //         fetch(`/user/${call.peer}`)
+  //           .then((res) => {
+  //             return res.json();
+  //           })
+  //           .then((data) => {
+  //             addVideoStream(video, userVideoStream, call.peer, data.name);
+  //           });
+  //       });
+  //       call.on("close", () => {
+  //         video.parentElement.remove();
+  //       });
+  //     });
+
+  //     socket.on("user-connected-screen", (userId) => {
+  //       console.log("user-connected");
+  //       connectToNewUser1(userId, stream);
+  //     });
+  //   });
+  // screenPeer.on("open", (id) => {
+  //   Screen_Peer_ID = id;
+  // });
+  // function connectToNewUser1(userId, stream) {
+  //   // set others peerid and send my stream
+  //   const call = screenPeer.call(userId, stream);
+  //   console.log(call);
+  //   const video = document.createElement("video");
+  //   call.on("stream", (userVideoStream) => {
+  //     fetch(`/user/${call.peer}`)
+  //       .then((res) => {
+  //         return res.json();
+  //       })
+  //       .then((data) => {
+  //         addVideoStream(video, userVideoStream, call.peer, data.name);
+  //       });
+  //   });
+  //   call.on("close", () => {
+  //     video.parentElement.remove();
+  //   });
+  //   peers[userId] = call;
+  // }
+}
+function asd() {
+  console.log(peers);
+}
+
+setInterval(() => {
+  const videoContainers = document.querySelectorAll("#video-wrapper");
+  videoContainers.forEach((videoContainer) => {
+    if (videoContainer.childNodes.length < 2) {
+      videoContainer.remove();
+    }
+  });
+}, 1);
+
+const eventAdd = (element) => {
+  element.addEventListener("click", videoClickEvent);
+};
+
+const videoClickEvent = (e) => {
+  const videoWrapper = e.target.parentElement;
+  console.log("Video CLick");
+  console.log(videoWrapper.classList.contains("zoom-video"));
+  if (!videoWrapper.classList.contains("zoom-video")) {
+    videoWrapper.classList.add("zoom-video");
+    const minimizeBtn = document.createElement("button");
+    minimizeBtn.classList.add("video-element-btn");
+    minimizeBtn.classList.add("video-minimize-btn");
+    minimizeBtn.innerHTML = `<i class="none-pointer fas fa-times fa-2x"></i>`;
+    videoWrapper.appendChild(minimizeBtn);
+    minimizeBtn.addEventListener("click", crossBtnClickEvent);
+  }
+};
+
+const crossBtnClickEvent = (e) => {
+  console.log(e.target);
+  const videoWrapper = e.target.parentElement;
+  if (videoWrapper.classList.contains("zoom-video")) {
+    videoWrapper.classList.remove("zoom-video");
+    e.target.removeEventListener("click", crossBtnClickEvent);
+    e.target.remove();
+  }
+};
+
+// External
+function stop() {
+  let enabled = myVideoStream.getVideoTracks()[0].enabled;
+  const btn = document.getElementById("video");
+  if (enabled) {
+    myVideoStream.getVideoTracks()[0].enabled = false;
+    btn.innerHTML = `<i class="fas fa-video-slash">`;
+  } else {
+    myVideoStream.getVideoTracks()[0].enabled = true;
+    btn.innerHTML = `<i class="fas fa-video">`;
+  }
+}
+
+function mute() {
+  const enabled = myVideoStream.getAudioTracks()[0].enabled;
+  const btn = document.getElementById("mute");
+  if (enabled) {
+    myVideoStream.getAudioTracks()[0].enabled = false;
+    btn.innerHTML = `<i class="fas fa-microphone-slash"></i>`;
+  } else {
+    myVideoStream.getAudioTracks()[0].enabled = true;
+    btn.innerHTML = `<i class="fas fa-microphone"></i>`;
+  }
+}
+const shareBtn = document.querySelector(".share-btn");
+shareBtn.addEventListener("click", (e) => {
+  e.target.parentElement.classList.toggle("active");
+});
+
+function call() {
+  const callBtn = document.getElementById("meeting-end");
+  const flag = callBtn.getAttribute("data");
+  if (flag == "false") {
+    console.log(Peer_ID);
+    callBtn.classList.remove("connect");
+    callBtn.setAttribute("data", "true");
+    socket.emit("join-room", ROOM_ID, Peer_ID, name);
+  }
+  if (flag == "true") {
+    location.replace(`/`);
+  }
+}
+// copy text
+const copyBtn = document.getElementById("copy");
+copyBtn.addEventListener("mousedown", (e) => {
+  const text = `https://digiclass.site/${ROOM_ID}`;
+  navigator.clipboard.writeText(text);
+  copyBtn.style.setProperty("--tooltip", '"copied"');
+});
+copyBtn.addEventListener("mouseout", (e) => {
+  copyBtn.style.setProperty("--tooltip", '"copy"');
+});
---
+++ server.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const app = express();
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
+const { ExpressPeerServer } = require("peer");
+const peerServer = ExpressPeerServer(server, {
+  debug: true,
+});
+const { v4: uuidV4 } = require("uuid");
+var users = {};
+app.use(express.json());
+app.use("/peerjs", peerServer);
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
+
+app.get("/:room", (req, res) => {
+  res.render("room", { roomId: req.params.room, screen: req.query.screen });
+});
+
+app.get("/user/:id", (req, res) => {
+  console.log(users[req.params.id]);
+  res.json({ name: users[req.params.id] });
+});
+
+app.post("/join-room", (req, res) => {
+  res.redirect(`/${req.body.room_id}`);
+});
+
+app.post("/create-room", (req, res) => {
+  res.redirect(`/${uuidV4()}`);
+});
+
+io.on("connection", (socket) => {
+  socket.to("join-room-screen", (roomId, userId, name) => {
+    console.log(name);
+    users[userId] = name;
+    socket.join(roomId);
+    socket.to(roomId).broadcast.emit("user-connected-screen", userId);
+
+    socket.on("disconnect", () => {
+      delete users.userId;
+      socket.to(roomId).broadcast.emit("user-disconnected", userId);
+    });
+  });
+  socket.on("join-room", (roomId, userId, name) => {
+    console.log(name);
+    users[userId] = name;
+    socket.join(roomId);
+    socket.to(roomId).broadcast.emit("user-connected", userId);
+
+    socket.on("disconnect", () => {
+      delete users.userId;
+      socket.to(roomId).broadcast.emit("user-disconnected", userId);
+    });
+  });
+});
+console.log(process.env.PORT);
+server.listen(process.env.PORT || 3000);
