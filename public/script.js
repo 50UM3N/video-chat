0320dd29ff41c679d14bb3182457d6e10466e7fe
Soumen Khara <soumen2015.s.k@gmail.com>
2021-01-19 18:21:42 +0530

    changing peer port

--- public/script.js
+++ public/script.js
@@ -27,7 +27,7 @@ const videoGrid = document.getElementById("video-grid");
 const myPeer = new Peer(undefined, {
   path: "/peerjs",
   host: "/",
-  port: "8080",
+  // port: "8080",
 });
 var Peer_ID;
 const myVideo = document.createElement("video");
