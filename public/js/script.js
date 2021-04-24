b7269cd092e02d11850e42c2ea25bc1f15a197b5
Soumen Khara <soumen2015.s.k@gmail.com>
2021-04-24 14:21:08 +0530

    Removigr port

--- public/js/script.js
+++ public/js/script.js
@@ -19,7 +19,7 @@ const videoGrid = document.getElementById("video-grid");
 const myPeer = new Peer(undefined, {
     path: "/peerjs",
     host: "/",
-    port: "3000",
+    // port: "3000",
 });
 var Peer_ID;
 const myVideo = document.createElement("video");
